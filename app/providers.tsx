4645bf9cb27baa9758e8610b2ffd694e4f5e643c
LimitLess16 <sasi70943292@gmail.com>
2026-08-28 17:11:14 +0530

    Fix next-themes script tag React hydration error

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,9 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
+import * as React from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
